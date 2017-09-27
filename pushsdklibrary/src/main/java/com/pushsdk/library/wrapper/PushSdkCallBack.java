9f1efdcc10f0950245a234f02c04975e3e46972d
xuxinwei <xuxinwei@shandagames.com>
2017-09-27 17:44:14 +0800

    upgrade readme

--- pushsdklibrary/src/main/java/com/pushsdk/library/wrapper/PushSdkCallBack.java
+++ pushsdklibrary/src/main/java/com/pushsdk/library/wrapper/PushSdkCallBack.java
@@ -4,9 +4,12 @@ import android.content.Context;
 
 /**
  * Created by xuxinwei on 2017/9/25.
+ * 推送监听接口，在注册后回调上报token，点击通知栏后监听点击事件
  */
 
 public interface PushSdkCallBack {
+    //点击通知栏
     void onNotificationMessageClicked(Context context, String message);
+    //上报token
     void upLoadToken(String token,int RomType);
 }
